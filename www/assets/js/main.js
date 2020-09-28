dca443db4612d52bfc53d49d12a1d234293ac425
Loan Cleris <>
2020-09-28 16:04:57 +0200

    Background and contact info fixed

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -55,7 +55,7 @@ sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__img', {delay: 400})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
